a1e40f75f83be827cddb939a5da30dcb91d2613f
lee-eojin <lejin0906@snu.ac.kr>
2025-10-02 13:37:43 +0900

    Unit-02 함수 스켈레톤 작성

---
+++ Unit-02/stringCalculator.js
@@ -0,0 +1,19 @@
+const DEFAULT_DELIMITER = /,|:/;
+
+function splitNumbers(text) {
+  // 여기에 구현하세요
+}
+
+function toNumber(str) {
+  // 여기에 구현하세요
+}
+
+function sum(numbers) {
+  // 여기에 구현하세요
+}
+
+function add(text) {
+  // 여기에 구현하세요
+}
+
+module.exports = { add, splitNumbers, toNumber, sum, DEFAULT_DELIMITER };
