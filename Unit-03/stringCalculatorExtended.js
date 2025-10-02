5f46e0993aaf8768a99b3d5f8d0742ce77c3adde
lee-eojin <lejin0906@snu.ac.kr>
2025-10-02 13:39:52 +0900

    Unit-03 함수 스켈레톤 작성

---
+++ Unit-03/stringCalculatorExtended.js
@@ -0,0 +1,23 @@
+const DEFAULT_DELIMITER = /,|:/;
+const CUSTOM_DELIMITER_PATTERN = /^\/\/(.)\n(.*)$/;
+
+function parseTokens(text) {
+  // 여기에 구현하세요
+}
+
+function toNumber(str) {
+  // 여기에 구현하세요
+}
+
+function sum(nums) {
+  // 여기에 구현하세요
+}
+
+function add(text) {
+  // 여기에 구현하세요
+}
+
+module.exports = {
+  add, parseTokens, toNumber, sum,
+  DEFAULT_DELIMITER, CUSTOM_DELIMITER_PATTERN
+};
