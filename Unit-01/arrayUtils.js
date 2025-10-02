8ef79689b70206e921ef555ddf89af686d2c044c
lee-eojin <lejin0906@snu.ac.kr>
2025-10-02 13:30:21 +0900

    Unit-01 함수 스켈레톤 작성

---
+++ Unit-01/arrayUtils.js
@@ -0,0 +1,9 @@
+function sum(arr) {
+  // 여기에 구현하세요
+}
+
+function max(arr) {
+  // 여기에 구현하세요
+}
+
+module.exports = { sum, max };
---
+++ Unit-01/reverseString.js
@@ -0,0 +1,5 @@
+function reverseString(str) {
+  // 여기에 구현하세요
+}
+
+module.exports = reverseString;
