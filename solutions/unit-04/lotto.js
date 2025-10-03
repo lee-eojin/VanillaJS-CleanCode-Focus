edc7620babba9a7194a4064ca9dbdc4f3d6d655e
lee-eojin <lejin0906@snu.ac.kr>
2025-10-03 18:43:11 +0900

    Unit-04 솔루션 작성중 (검증단계)

---
+++ solutions/unit-04/lotto.js
@@ -0,0 +1,38 @@
+const LOTTO_MIN = 1;
+const LOTTO_MAX = 45;
+const LOTTO_COUNT = 6;
+
+const defaultRandomGenerator = {
+  generateNumber: (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+};
+
+function generateRandomNumber(min, max, randomGenerator = defaultRandomGenerator) {
+  return randomGenerator.generateNumber(min, max);
+}
+
+function generateLottoNumbers(randomGenerator = defaultRandomGenerator) {
+  const numbers = new Set();
+
+  while (numbers.size < LOTTO_COUNT) {
+    const num = generateRandomNumber(LOTTO_MIN, LOTTO_MAX, randomGenerator);
+    numbers.add(num);
+  }
+
+  return Array.from(numbers);
+}
+
+function sortNumbers(numbers) {
+  // 여기에 구현하세요
+}
+
+module.exports = {
+  generateRandomNumber,
+  generateLottoNumbers,
+  sortNumbers,
+  defaultRandomGenerator,
+  LOTTO_MIN,
+  LOTTO_MAX,
+  LOTTO_COUNT
+};
