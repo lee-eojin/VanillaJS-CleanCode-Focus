0033be62c7c4d3fb2f3ce1aea276acb9ffc5fc0f
lee-eojin <lejin0906@snu.ac.kr>
2025-10-07 17:28:03 +0900

    unit 파일 순서 통일

--- solutions/unit-06/sortAndSearch.js
+++ solutions/unit-06/sortAndSearch.js
@@ -13,24 +13,7 @@ function bubbleSort(arr) {
 }
 
 function binarySearch(sortedArr, target) {
-  let left = 0;
-  let right = sortedArr.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-
-    if (sortedArr[mid] === target) {
-      return mid;
-    }
-
-    if (sortedArr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-
-  return -1;
 }
 
 module.exports = {
