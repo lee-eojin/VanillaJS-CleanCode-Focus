69d6da118889044ca20cbaf055e1714badd17b4b
lee-eojin <lejin0906@snu.ac.kr>
2025-10-11 17:34:36 +0900

    feat: Unit-08 Stack 클래스 구현 및 나머지 문제 추가
    나머지 문제도 풀고 바로 추가할 예정

---
+++ solutions/unit-08/bracket.js
@@ -0,0 +1,6 @@
+function isValidBracket(str) {
+  // 여기에 구현하세요
+  // 올바른 괄호면 true, 아니면 false
+}
+
+module.exports = { isValidBracket };
---
+++ solutions/unit-08/queue.js
@@ -0,0 +1,27 @@
+class Queue {
+  constructor() {
+    // 여기에 구현하세요
+  }
+
+  enqueue(item) {
+    // 여기에 구현하세요
+  }
+
+  dequeue() {
+    // 여기에 구현하세요
+  }
+
+  front() {
+    // 여기에 구현하세요
+  }
+
+  isEmpty() {
+    // 여기에 구현하세요
+  }
+
+  size() {
+    // 여기에 구현하세요
+  }
+}
+
+module.exports = Queue;
